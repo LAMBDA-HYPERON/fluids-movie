ad7cde767734291db721260f8407af777a2422a6
Wasin Chaivaranont <Wasin Chaivaranont>
2015-11-24 13:31:43 +1100

    make_movie: add main function

---
+++ make_movie.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+
+"""
+This script makes a fluid animation using NetCDF data.
+"""
+
+def main ():
+	print "I'm the main function."
+	return True
+
+if __name__=="__main__":
+	sys.exit(main())
