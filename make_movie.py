a4d82b5e5494728b93a99bc3243d90af885546e0
Wasin Chaivaranont <Wasin Chaivaranont>
2015-11-24 13:55:16 +1100

    make_move: add argparse to main function

--- make_movie.py
+++ make_movie.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
+
+import numpy as np
+import netCDF4 as nc
 
 """
 This script makes a fluid animation using NetCDF data.
 """
 
 def main ():
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument('input_file',help="Input file containing our data")
+	parser.add_argument('field',help="Data field to animate")
+	args = parser.parse_args()
+	
 	print "I'm the main function."
 	return True
 
